a52e8936a49c8d054f1fb8bfd5a7c1109d8cba46
Mario Ruiz <MarioRuizS@gmail.com>
2019-09-05 22:49:59 +0000

    select fields tests

---
+++ spec/nice_hash_select_fields_spec.rb
@@ -0,0 +1,25 @@
+require 'nice_hash'
+
+RSpec.describe NiceHash, "#select_fields" do
+  it "returns the select fields" do
+    res = NiceHash.select_fields(@hash)
+    pf = [[:city, :correct]]
+    expect(res).to eq (pf)
+  end
+  it "returns the select fields when using class Hash" do
+    res = @hash.select_fields
+    pf = [[:city, :correct]]
+    expect(res).to eq (pf)
+  end
+  it "returns the select fields with selected keys" do
+    res = NiceHash.select_fields(@hash, :correct)
+    pf = [[:city]]
+    expect(res).to eq (pf)
+  end
+  #   my_hash.patterns(:correct)
+  it "returns the pattern fields with selected keys using Hash class" do
+    res = @hash.select_fields(:correct)
+    pf = [[:city]]
+    expect(res).to eq (pf)
+  end
+end
